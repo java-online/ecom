6c2be6401e80c1f2f6742fc6be3bb32584eff934
java-online <65293221+java-online@users.noreply.github.com>
2020-05-14 20:53:02 +1000

    Initial Api Project
    Backen api for the project

---
+++ src/main/java/ecom/prt/in/EcomportalApplication.java
@@ -0,0 +1,13 @@
+package ecom.prt.in;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomportalApplication.class, args);
+	}
+
+}
